bf16e87902bb0f43b5eca470364b55e04ef2fb61
kalonjis <kalonjistephane@gmail.com>
2021-10-19 12:12:16 +0200

    js: clic actions - function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,23 @@ const conditionsVictoire = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
 
 //Messages
 const gagne = () =>`Lejoueur ${joueurActif} a gagné`;
 const egalité = () => "Egalité";
 const tourJoueur = () => `c'est au tour du joueur ${joueurActif}`;
 
-statut.innerHTML = tourJoueur()
+statut.innerHTML = tourJoueur();
+
+//Action
+document.querySelectorAll(".case").forEach(cell => cell.addEventListener('click', gestionClicCase));
+
+document.querySelector('#recommencer').addEventListener('click', recommencer);
+
+function gestionClicCase() {
+    console.log(this)
+}
+function recommencer(){
+    console.log("recommencer")
+}
